f988b0df82f0ce77eb49d4019132d0e2ff3a6885
Akshay Shetty <84218281+akshayshetty744@users.noreply.github.com>
2022-09-28 23:20:04 +0530

    pairs

--- object-methods.js
+++ object-methods.js
@@ -17,8 +17,7 @@ function mapObject(obj, cb) {
 }
 
 function pairs(obj) {
-  // Convert an object into a list of [key, value] pairs.
-  // http://underscorejs.org/#pairs
+  return  Object.entries(obj)
 }
 
 /* STRETCH PROBLEMS */
