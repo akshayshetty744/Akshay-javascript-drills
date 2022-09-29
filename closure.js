93d8c97d23b91063717d8c2c06c05c0b64190526
Akshay Shetty <84218281+akshayshetty744@users.noreply.github.com>
2022-09-29 18:43:49 +0530

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,7 +1,9 @@
 function counterFactory() {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  let a=0;
+  return {
+    increment:()=> (++a),
+    decrement: ()=> (a - 1),
+  };
 }
 
 function limitFunctionCallCount(cb, n) {
