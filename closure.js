1e5992936dc4ad98df2b6464d144a2ff5efc3ff3
Akshay Shetty <84218281+akshayshetty744@users.noreply.github.com>
2022-09-29 18:44:46 +0530

    Update closure.js

--- closure.js
+++ closure.js
@@ -16,10 +16,10 @@ function limitFunctionCallCount(cb, n) {
 }
 
 function cacheFunction(cb) {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+   let cache = {};
+  return (a) => {
+    if (Object.prototype.hasOwnProperty.call(cache, a)) return cache[a];
+    cache[a] = cb(a);
+    return cache[a];
+  };
 }
